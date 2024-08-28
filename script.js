54bcb3d7287410f8d0125ffec858ea1432e2cd68
GiorgioDordo <djole6789@gmail.com>
2024-08-28 15:51:14 +0200

    milestone 2

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 // get elements from dom
 const grid = document.getElementById('grid');
+const play = document.getElementById('play-btn');
 
 const rows = 10;
 const cols = 10;
 const totalCells = rows * cols;
 
 // create cells 
-for (let i = 0; i < totalCells; i++) {
-    const cell = document.createElement('div');
-    cell.className = 'cell';
+play.addEventListener('click', function(e){
+    e.preventDefault();
 
-    grid.appendChild(cell);
-}
+    for (let i = 0; i < totalCells; i++) {
+        const cell = document.createElement('div');
+        cell.className = 'cell';
+    
+        grid.appendChild(cell);
+    }
+    
+})
 
